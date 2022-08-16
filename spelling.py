0fd517c144b859df050bdb4a51f2b1d1eda5bbdb
keithbecker <keithloganbecker94@gmail.com>
2022-08-16 16:48:24 -0600

    main.py

--- spelling.py
+++ spelling.py
@@ -7,3 +7,14 @@ def is_word_spelled_correct(word):
     pass
 
 
+def check_list_for_spellling(words):
+    for word in words:
+        is_correct = is_word_spelled_correct(word)
+        if is_correct:
+            pass
+        else:
+            raise Exception(f"word {word} is not spelled correct")
+
+
+
+
