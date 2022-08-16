3778c1f0ccf858f4c60dc6df74b4aee845b3cc08
keithbecker <keithloganbecker94@gmail.com>
2022-08-16 17:07:13 -0600

    json.py

--- json.py
+++ json.py
@@ -4,7 +4,7 @@ import json
 def parse_content_into_arr():
     f = open("content.json", "r")
     str = f.read()
-    arr = json.loads(str).decode("utf-8")
+    arr = json.loads(str)
     return arr
 
 
