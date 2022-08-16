596f7c2d62e2f211f40cb6e6fc0b4ea92ccd61ea
keithbecker <keithloganbecker94@gmail.com>
2022-08-16 16:44:39 -0600

    main.py

---
+++ spelling.py
@@ -0,0 +1,9 @@
+# use this library
+from spellchecker import SpellChecker
+
+
+# use examples from https://www.geeksforgeeks.org/spelling-checker-in-python/ to write function
+def is_word_spelled_correct(word):
+    pass
+
+
