4c67acd8849844cfd12f2db542e873c765015d32
keithbecker <keithloganbecker94@gmail.com>
2022-08-16 17:56:45 -0600

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from content import content_as_array
 from spelling import check_list_for_spelling
 
+
 def main():
     content_arr = content_as_array()
-    check_list_for_spelling(content_arr)
+
+    try:
+        check_list_for_spelling(content_arr)
+    except Exception as e:
+        err_message = str(e)
+        print(err_message)
+        quit(1)
+
+
+
 
 
 
